10eebdf2dd8cfb2cb4571373e1c21976e85d4902
Tarandeep Singh Thamrait <tthamrai@uci.edu>
2021-05-28 12:25:30 -0700

    Added Loader component

---
+++ src/components/Loader.js
@@ -0,0 +1,10 @@
+const Loader = () => {
+  return (
+    <div className="loader">
+      <img src="/spinner.gif" alt="Loading"/>
+      <h1>Fetching Data</h1>
+    </div>
+  )
+}
+
+export default Loader
